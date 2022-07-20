7bafa7f2bd82fe49b16c1ef1b0bcd3988bf9c85d
LasloMagnusen <100539534+LasloMagnusen@users.noreply.github.com>
2022-07-20 12:32:18 +0300

    Add files via upload

---
+++ minting-dapp/src/scripts/lib/NftContractType.ts
@@ -0,0 +1,7 @@
+// The name below ("TestDeploy") should match the name of your Solidity contract.
+// It can be updated using the following command:
+// yarn rename-contract NEW_CONTRACT_NAME
+// Please DO NOT change it manually!
+import { TestDeploy as NftContractType } from '../../../../smart-contract/typechain/index';
+
+export default NftContractType;
